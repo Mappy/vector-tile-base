9992eae5c7561c74eeb21fb42c88decfe63eb540
Stephen Kinger <stephen.kinger@mappy.com>
2025-08-04 16:33:41 +0200

    for trying purpose

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vector_tile_base",
-    version="1.0.2",
+    version="1.0.3",
     description="Python implementation of Mapbox vector tiles",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
